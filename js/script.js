df4e7752edd4034f53e16d9cd59d91832287fe6b
rolo-coding <kay.sothearo2019@rupp.edu.kh>
2022-09-22 22:44:52 +0700

    bad bad codes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-window.addEventListener("scroll", () => {
-  const header = document.querySelector(".header")
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('.header')
   const scrollY = window.scrollY
   if (scrollY > 60) {
-    header.style.backgroundColor = "#100f0f"
+    header.style.backgroundColor = '#100f0f'
   } else {
-    header.style.backgroundColor = "rgba(0, 0, 0, 0)"
+    header.style.backgroundColor = 'rgba(0, 0, 0, 0)'
   }
 })
 
-const API_KEY = "86783762237ff3e97be67f3473685c59"
-const BASE_PATH = "https://api.themoviedb.org/3"
+const API_KEY = '86783762237ff3e97be67f3473685c59'
+const BASE_PATH = 'https://api.themoviedb.org/3'
 
 const getMovies = async () => {
   return await Promise.all(
-    ["movie", "tv"].map(id =>
+    ['movie', 'tv'].map(id =>
       fetch(`${BASE_PATH}/${id}/top_rated?api_key=${API_KEY}`).then(response =>
         response.json()
       )
@@ -36,7 +36,7 @@ const getMoviesWithGenre = async (id, genreId) => {
 }
 
 const getMovieImage = (path, format) => {
-  return `https://image.tmdb.org/t/p/${format ? format : "original"}${path}`
+  return `https://image.tmdb.org/t/p/${format ? format : 'original'}${path}`
 }
 
 const getDetail = async data => {
@@ -46,9 +46,9 @@ const getDetail = async data => {
 }
 
 const createBanner = data => {
-  const banner = document.querySelector(".banner")
-  const title = banner.querySelector(".title")
-  const overview = banner.querySelector(".overview")
+  const banner = document.querySelector('.banner')
+  const title = banner.querySelector('.title')
+  const overview = banner.querySelector('.overview')
 
   banner.style.backgroundImage = `linear-gradient(to right, #100f0f 20%,  #0002), 
     url(${getMovieImage(data.backdrop_path)})`
@@ -57,32 +57,32 @@ const createBanner = data => {
 }
 
 const createCard = data => {
-  const card = document.createElement("div")
-  document.querySelector("main").appendChild(card)
-  card.classList.add("card")
+  const card = document.createElement('div')
+  document.querySelector('main').appendChild(card)
+  card.classList.add('card')
   card.innerHTML = `<img src="${getMovieImage(data.poster_path)}">`
   card.innerHTML += `<p>${data.overview}</p>`
 }
 
 const createModal = (e, data) => {
-  const series = document.querySelector(".modal-series")
+  const series = document.querySelector('.modal-series')
   series.style.opacity = 1
   series.style.backgroundImage = `linear-gradient(to right, #191919 50%,  #0002),
     url(${getMovieImage(data.backdrop_path)})`
   series.innerHTML = `<img src="${e.target.src}">`
-  series.innerHTML += "<span>&times;</span>"
+  series.innerHTML += '<span>&times;</span>'
   series.innerHTML += `<div><h3>${data.name}</h3><p>${data.overview}</p></div>`
 
-  let detail = series.querySelector("div")
+  let detail = series.querySelector('div')
 
-  let ul = document.createElement("ul")
+  let ul = document.createElement('ul')
   getDetail(data).then(data => {
-    const releaseYear = document.createElement("li")
-    releaseYear.innerText = `${data.first_air_date.split("-")[0]}`
+    const releaseYear = document.createElement('li')
+    releaseYear.innerText = `${data.first_air_date.split('-')[0]}`
     ul.appendChild(releaseYear)
 
     data.genres.map((genre, i, arr) => {
-      let li = document.createElement("li")
+      let li = document.createElement('li')
       li.innerText = arr.length - 1 === i ? `${genre.name}` : `${genre.name},`
       ul.appendChild(li)
     })
@@ -90,13 +90,13 @@ const createModal = (e, data) => {
   detail.appendChild(ul)
 
   /* rating */
-  let rating = document.createElement("div")
+  let rating = document.createElement('div')
   rating.innerHTML += `<span>${data.vote_average}/10</span>`
 
-  let starsOuter = document.createElement("div")
-  let starsInner = document.createElement("div")
-  starsOuter.classList.add("stars-outer")
-  starsInner.classList.add("stars-inner")
+  let starsOuter = document.createElement('div')
+  let starsInner = document.createElement('div')
+  starsOuter.classList.add('stars-outer')
+  starsInner.classList.add('stars-inner')
   starsOuter.appendChild(starsInner)
   rating.appendChild(starsOuter)
   detail.appendChild(rating)
@@ -106,79 +106,84 @@ const createModal = (e, data) => {
   starPercentage = Math.round(starPercentage / 10) * 10
   starsInner.style.width = `${starPercentage}%`
 
-  series.querySelector("span").addEventListener("click", () => {
-    modal.classList.remove("modal-active")
+  series.querySelector('span').addEventListener('click', () => {
+    modal.classList.remove('modal-active')
     series.style.opacity = 0
   })
 }
 
-const modal = document.querySelector(".modal")
+const modal = document.querySelector('.modal')
 
 const createMovie = data => {
-  const movie = document.createElement("div")
+  const movie = document.createElement('div')
   movies.appendChild(movie)
-  movie.classList.add("movie", "swiper-slide")
+  movie.classList.add('movie', 'swiper-slide')
   movie.innerHTML = `<img src="${getMovieImage(data.poster_path)}">`
 
-  movie.addEventListener("click", e => {
-    modal.classList.add("modal-active")
+  movie.addEventListener('click', e => {
+    modal.classList.add('modal-active')
     createModal(e, data)
   })
 }
 
-const homePath = document.querySelector("#home a")
-const tvPath = document.querySelector("#series a")
+const homePath = document.querySelector('#home a')
+const tvPath = document.querySelector('#series a')
 
 getMovies().then(response => {
   const movie = response[0]
   const tv = response[1]
 
-  homePath.classList.contains("active") && createBanner(movie.results[13])
-  if (tvPath.classList.contains("active")) {
+  homePath.classList.contains('active') && createBanner(movie.results[13])
+  if (tvPath.classList.contains('active')) {
     createBanner(tv.results[11])
     createCard(tv.results[11])
     tv.results.map(movie => createMovie(movie))
 
-    getGenres("tv").then(({ genres }) =>
+    getGenres('tv').then(({ genres }) => {
+      const ul = document.querySelector('ul.genres')
       genres.splice(1, 8).map(genre => {
-        const li = document.createElement("li")
+        const li = document.createElement('li')
+        ul.appendChild(li)
         li.innerHTML = `<a href="#">${genre.name}</a>`
-        li.addEventListener("click", () => {
-          getMoviesWithGenre("tv", genre.id).then(data => {
-            movies.innerHTML = ""
+        li.addEventListener('click', () => {
+          ul.querySelectorAll('li').forEach(
+            li =>
+              li.classList.contains('active') && li.classList.remove('active')
+          )
+          li.classList.add('active')
+          getMoviesWithGenre('tv', genre.id).then(data => {
+            movies.innerHTML = ''
             data.results.map(poster => createMovie(poster))
-            li.classList.add('active')
           })
         })
-        document.querySelector("ul.genres").appendChild(li)
       })
-    )
+    })
   }
 })
 
 /* search */
-const search = document.querySelector("form.search")
-const searchBtn = search.querySelector("svg")
+const search = document.querySelector('form.search')
+const searchBtn = search.querySelector('svg')
 const input = document.querySelector('input[name="title"]')
 
 let clicked = false
-searchBtn.addEventListener("click", () => {
+searchBtn.addEventListener('click', () => {
   clicked = !clicked
   if (clicked) {
-    input.style.transform = "scaleX(1)"
-    searchBtn.style.transform = "translateX(-190px)"
+    input.style.transform = 'scaleX(1)'
+    searchBtn.style.transform = 'translateX(-190px)'
   } else {
-    input.style.transform = "scaleX(0)"
-    searchBtn.style.transform = "translateX(0)"
+    input.style.transform = 'scaleX(0)'
+    searchBtn.style.transform = 'translateX(0)'
   }
 })
 
-search.addEventListener("submit", e => {
+search.addEventListener('submit', e => {
   e.preventDefault()
-  movies.innerHTML = ""
+  movies.innerHTML = ''
 
   const checkTitle = input.value.toLowerCase()
-  if (checkTitle === "") return
+  if (checkTitle === '') return
 
   getMovies().then(results => {
     results.map(movie => {
@@ -188,5 +193,5 @@ search.addEventListener("submit", e => {
       }
     })
   })
-  input.value = ""
+  input.value = ''
 })
