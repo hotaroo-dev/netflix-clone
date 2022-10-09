14e00563863541c0c03a63ca4fa49197e20cb3bc
rolo-coding <kay.sothearo2019@rupp.edu.kh>
2022-10-09 23:15:24 +0700

    fix(genre): remove genre navbar

--- js/series.js
+++ js/series.js
@@ -5,26 +5,6 @@ getMovies('tv').then(res => {
   swiperInit(0)
   const movieRow = document.querySelector('#row0')
   tv.map(movie => createMovie(movie, movieRow))
-
-  getGenres('tv').then(({ genres }) => {
-    const ul = document.querySelector('ul.genres')
-    genres.splice(1, 8).map(genre => {
-      const li = document.createElement('li')
-      ul.appendChild(li)
-      li.innerHTML = `<a href="#">${genre.name}</a>`
-      li.addEventListener('click', e => {
-        e.preventDefault()
-        const activeEl = ul.querySelector('li.active')
-        activeEl && activeEl.classList.remove('active')
-        li.classList.add('active')
-
-        getMoviesWithGenre('tv', genre.id).then(data => {
-          movies.innerHTML = ''
-          data.results.map(poster => createMovie(poster))
-        })
-      })
-    })
-  })
 })
 
 document
