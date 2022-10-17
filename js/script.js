0c3019cf9e916ce8118724e59a09fccf2048c822
rolo-coding <kay.sothearo2019@rupp.edu.kh>
2022-10-17 13:32:47 +0700

    fix(bug): fixed bug

--- js/script.js
+++ js/script.js
@@ -72,7 +72,6 @@ const getMovieWithGenres = (type, id) => {
     )
   )
 }
-
 /* end utils function */
 
 const totalRating = 10
@@ -132,8 +131,7 @@ const paintCard = (data, card, type) => {
       }`
     ul.appendChild(releaseYear)
 
-    genres = window.innerWidth < 540 ? genres.slice(0, 2) : genres
-    genres.map((genre, i, arr) => {
+    genres.map((genre, arr) => {
       let li = document.createElement('li')
       const name = genre.name.split(' & ')[0]
       li.innerText = `${name}`
@@ -154,9 +152,11 @@ const createCard = data => {
   card.classList.add('card')
   paintCard(data, card, data.type)
 
-  const deleteBtn =
+  const deleteBtn = document.createElement('button')
+  deleteBtn.innerHTML =
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>'
-  card.innerHTML += deleteBtn
+
+  card.append(deleteBtn)
   card.lastChild.addEventListener('click', e => deleteMovie(e, data))
 
   document.querySelector('.movie-list .wrapper').appendChild(card)
@@ -204,9 +204,11 @@ const createModal = data => {
     if (main.classList.contains('movie-list')) {
       let titles = main.querySelectorAll('.card > div > h3')
       let isMovie = false
-      titles.forEach(title => {
-        if (title.textContent === data.title) isMovie = true
+      titles.forEach(t => {
+        const title = data.title || data.name
+        if (t.textContent === title) isMovie = true
       })
+      console.log(isMovie)
       !isMovie && createCard(data)
     }
   })
@@ -231,7 +233,7 @@ const createMovie = (data, el) => {
 }
 
 function deleteMovie(e, data) {
-  const card = e.target.parentElement.parentElement
+  const card = e.currentTarget.parentElement
   card.classList.add('fall')
   card.addEventListener('transitionend', () => card.remove())
   removeLocalMovie(data)
